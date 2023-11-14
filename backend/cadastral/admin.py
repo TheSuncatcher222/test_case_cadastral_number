2f77bf7fbd0be1107c3e2f41034c61bf7cba7cad
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-11-15 01:34:36 +0300

    Add LogsHistory

--- backend/cadastral/admin.py
+++ backend/cadastral/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from backend.app_data import ADMIN_LIST_PER_PAGE
-from cadastral.models import CadastralNumber
+from cadastral.models import CadastralNumber, LogsHistory
 
 
 @admin.register(CadastralNumber)
@@ -50,5 +50,51 @@ class CadastralNumberAdmin(admin.ModelAdmin):
         'number',
         'latitude',
         'longitude',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(LogsHistory)
+class LogsHistoryAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django
+    для модели LogsHistory.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - id (id)
+            - путь HTTP запроса (path)
+            - метод HTTP запроса (method)
+            - JSON запроса (request_data)
+            - статус ответа (status_code)
+            - JSON ответа (response_data)
+            - кадастровый номер (cadastral)
+            - дата и время запроса (datetime)
+        - list_filter (tuple) - список фильтров:
+            - путь HTTP запроса (path)
+            - метод HTTP запроса (method)
+            - статус ответа (status_code)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - дата и время запроса (datetime)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'path',
+        'method',
+        'request_data',
+        'status_code',
+        'response_data',
+        'cadastral',
+        'datetime',
+    )
+    list_filter = (
+        'path',
+        'method',
+        'status_code',
+        'cadastral',
+    )
+    search_fields = (
+        'datetime',
     )
     list_per_page = ADMIN_LIST_PER_PAGE
--- backend/cadastral/models.py
+++ backend/cadastral/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-from backend.app_data import CADASTRAL_NUMBER_LEN
+from backend.app_data import (
+    CADASTRAL_NUMBER_LEN, HTTP_METHOD_NAME_MAX_LEN, URL_PATH_MAX_LEN,
+)
 from cadastral.validators import (
     validate_cadastral_number,
     validate_cadastral_lat,
@@ -39,3 +41,43 @@ class CadastralNumber(models.Model):
 
     def str(self):
         return self.number
+
+
+class LogsHistory(models.Model):
+    """Класс представления истории запросов и ответов к серверу."""
+
+    path = models.CharField(
+        verbose_name='Путь HTTP запроса',
+        max_length=URL_PATH_MAX_LEN,
+    )
+    method = models.CharField(
+        verbose_name='Метод HTTP запроса',
+        max_length=HTTP_METHOD_NAME_MAX_LEN,
+    )
+    request_data = models.TextField(
+        verbose_name='JSON запроса',
+    )
+    status_code = models.IntegerField(
+        verbose_name='Статус ответа',
+    )
+    response_data = models.TextField(
+        verbose_name='JSON ответа',
+    )
+    # INFO: не связываю с моделью CadastralNumber, так как тут содержатся
+    #       данные, указанные в HttpRequest.body, которые могут быть ошибочные.
+    cadastral = models.CharField(
+        verbose_name='Кадастровый номер',
+        max_length=CADASTRAL_NUMBER_LEN,
+    )
+    datetime = models.DateTimeField(
+        'Дата совершения запроса',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'HTTP запрос'
+        verbose_name_plural = 'История HTTP запросов'
+
+    def str(self):
+        return f'{self.method} {self.path}: {self.status_code}'
