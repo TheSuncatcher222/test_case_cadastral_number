5bf3fd871c4aa4097a606bd4f6dc30ff5eb5f2b6
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-11-16 13:24:05 +0300

    Update CADASTRAL_NUMBER_PATTERN

--- backend/cadastral/validators.py
+++ backend/cadastral/validators.py
@@ -2,7 +2,7 @@ import re
 
 from django.core.exceptions import ValidationError
 
-CADASTRAL_NUMBER_PATTERN: str = r'^\d{2}:\d{2}:\d{6}:\d{3}$'
+CADASTRAL_NUMBER_PATTERN: str = r'^\d{2}:\d{2}:\d{6}:\d{2}$'
 CADASTRAL_NUMBER_ERR: str = (
     'Укажите корректный кадастровый номер вида XX:XX:XXXXXX:XX.'
 )
